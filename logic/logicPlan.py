60957972daaa6522f957578a573f6805e988ef5b
PhongNguyenTien <99393835+PhongNguyenTien@users.noreply.github.com>
2023-10-30 08:26:46 +0700

    done q3 p2

--- logic/logicPlan.py
+++ logic/logicPlan.py
@@ -242,6 +242,8 @@ def pacmanSuccessorAxiomSingle(x: int, y: int, time: int, walls_grid: List[List[
         return None
     
     "*** BEGIN YOUR CODE HERE ***"
+    '''Current <==> (previous position at time t-1) & (took action to move to x, y)'''
+    return PropSymbolExpr(pacman_str, x, y, time=now) % disjoin(possible_causes)
     util.raiseNotDefined()
     "*** END YOUR CODE HERE ***"
 
@@ -313,6 +315,32 @@ def pacphysicsAxioms(t: int, all_coords: List[Tuple], non_outer_wall_coords: Lis
     pacphysics_sentences = []
 
     "*** BEGIN YOUR CODE HERE ***"
+    position_time = []
+    for x, y in all_coords:
+        #  If a wall is at (x, y) --> Pacman is not at (x, y)
+        pacphysics_sentences.append(PropSymbolExpr(wall_str, x, y) >> ~PropSymbolExpr(pacman_str, x, y, time=t))
+        if (x, y) in non_outer_wall_coords:
+            position_time.append(PropSymbolExpr(pacman_str, x, y, time=t))
+    # - Pacman is at exactly one of the squares at timestep t.
+    pacphysics_sentences.append(exactlyOne(position_time))
+    
+    # - Pacman takes exactly one action at timestep t.
+    action_time = []
+    for direction in DIRECTIONS:
+        action_time.append(PropSymbolExpr(direction, time=t))
+    pacphysics_sentences.append(exactlyOne(action_time))
+    
+    # - Results of calling sensorModel(...), unless None.
+    if sensorModel is not None:
+        pacphysics_sentences.append(sensorModel(t, non_outer_wall_coords))
+    
+    #  - Results of calling successorAxioms(...), describing how Pacman can end in various 
+    #       locations on this time step. Consider edge cases. Don't call if None.
+    if t > 0 and successorAxioms is not None:
+        pacphysics_sentences.append(successorAxioms(t, walls_grid, non_outer_wall_coords))
+        
+    return conjoin(pacphysics_sentences)
+    
     util.raiseNotDefined()
     "*** END YOUR CODE HERE ***"
 
@@ -329,7 +357,7 @@ def checkLocationSatisfiability(x1_y1: Tuple[int, int], x0_y0: Tuple[int, int], 
         - problem = an instance of logicAgents.LocMapProblem
     Note:
         - there's no sensorModel because we know everything about the world
-        - the successorAxioms should be allLegalSuccessorAxioms where needed
+        - the successorAxioms should be all LegalSuccessorAxioms where needed
     Return:
         - a model where Pacman is at (x1, y1) at time t = 1
         - a model where Pacman is not at (x1, y1) at time t = 1
@@ -347,6 +375,24 @@ def checkLocationSatisfiability(x1_y1: Tuple[int, int], x0_y0: Tuple[int, int], 
     KB.append(conjoin(map_sent))
 
     "*** BEGIN YOUR CODE HERE ***"
+    # add pacphysicsAxioms at t = 0 to KB
+    KB.append(pacphysicsAxioms(0, all_coords, non_outer_wall_coords, walls_grid, successorAxioms=allLegalSuccessorAxioms))
+    # add pacphysicsAxioms at t = 1 to KB
+    KB.append(pacphysicsAxioms(1, all_coords, non_outer_wall_coords, walls_grid, successorAxioms=allLegalSuccessorAxioms))
+
+    # Pacman located (x0, y0) at t = 0
+    KB.append(PropSymbolExpr(pacman_str, x0, y0, time=0))
+    # Pacman take action (action0) at t = 0
+    KB.append(PropSymbolExpr(action0, time=0))
+    # Pacman take action (action1) at t = 1 (to ensure match with autograder solution)
+    KB.append(PropSymbolExpr(action1, time=1))
+    
+    # - a model where Pacman is at (x1, y1) at time t = 1
+    model1 = findModel(conjoin(KB) & PropSymbolExpr(pacman_str, x1, y1, time=1))
+    # - a model where Pacman is not at (x1, y1) at time t = 1
+    model2 = findModel(conjoin(KB) & ~PropSymbolExpr(pacman_str, x1, y1, time=1))
+
+    return model1, model2
     util.raiseNotDefined()
     "*** END YOUR CODE HERE ***"
 
